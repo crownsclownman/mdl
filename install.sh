4b6586e1c56024ba19a165bb81d4025ad40ecb9c
ccm <theodore.machine@hotmail.com>
2026-07-16 13:36:11 +0500

    Update: 2026-07-16 13:36:11

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ REPO="crownsclownman/mdl"
 URL="https://github.com/$REPO/releases/latest/download/mdlcc"
 INSTALL_PATH="/usr/local/bin/mdlcc"
 
-curl -L $URL -o /tmp/mdlcc
-mv /tmp/mdlcc $INSTALL_DIR
-chmod +x $INSTALL_DIR
+curl -sS -L $URL -o /tmp/mdlcc
+mv /tmp/mdlcc $INSTALL_PATH
+chmod +x $INSTALL_PATH
 
-echo "Successfully installed to $INSTALL_DIR"
+echo "Successfully installed to $INSTALL_PATH"
